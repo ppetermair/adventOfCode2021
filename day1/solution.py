f27a7fb46924aaa9955c209379ee0f1b07e9ebad
Patrick Petermair <patrick.petermair@gmail.com>
2021-12-02 11:30:08 +0100

    day1

--- day1/solution.py
+++ day1/solution.py
@@ -6,15 +6,21 @@ def main():
     increments = 0
     measurements = get_input()
     for index, measurement in enumerate(measurements):
-        #print("{} reading: {}".format(index, measurement))
         if index == 0:
-            print("{} (N/A - no previous measurement)".format(measurement))            
+            continue            
         elif int(measurements[index-1]) < int(measurement):
-            print("{} {}".format(measurement, "(increased)"))
             increments+=1
-        else:
-            print("{} {}".format(measurement, "(decreased)"))
-    print("{} increments".format(increments))
+    print("{} increments Part I".format(increments))
+
+    increments = 0
+    for index, measurement in enumerate(measurements):
+        if index < 2:
+            continue
+        current_group = int(measurement) + int(measurements[index-1]) + int(measurements[index-2])
+        previous_group = int(measurements[index-1]) + int(measurements[index-2]) + int(measurements[index-3])
+        if previous_group < current_group:
+            increments+=1
+    print("{} increments Part II".format(increments))
 
 def get_input():
     with open(path.join(path.dirname(__file__), 'input')) as f:
